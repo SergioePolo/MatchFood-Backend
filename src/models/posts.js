56bd88146509276953e3402c84fa30e88fb4abfa
Spolo <sergioepolo@gmail.com>
2025-08-29 20:22:29 -0500

    [update] creacion modelo posts

---
+++ src/models/posts.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const postSchema = new Schema({
+    Image:{
+        type: String,
+        required: required
+    },
+    comment:{
+        type: String,
+    },
+    userId: {
+        type: String,
+        required: required
+    },
+    restaurantId: {
+        type: String,
+        required: required
+    }
+})
